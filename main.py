f210872dec82e6ef9f030c035ff7e06777a53960
James Vu <vuducnghia1996@gmail.com>
2021-09-21 15:47:32 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,32 +1,29 @@
 import os
-import re
 import zipfile
 
-import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 import tensorflow_datasets as tfds
-import matplotlib.pyplot as plt
 from utils.preprocessing import preprocess_data
 from utils.encode_label import LabelEncoder
 from model.model import setup_callback, ODHyperModel
 from keras_tuner import RandomSearch
 
 
-# url = "https://github.com/srihari-humbarwadi/datasets/releases/download/v0.1.0/data.zip"
-# filename = os.path.join(os.getcwd(), "data.zip")
-# keras.utils.get_file(filename, url)
-#
-#
-# with zipfile.ZipFile("data.zip", "r") as z_fp:
-#     z_fp.extractall("./")
+url = "https://github.com/srihari-humbarwadi/datasets/releases/download/v0.1.0/data.zip"
+filename = os.path.join(os.getcwd(), "data.zip")
+keras.utils.get_file(filename, url)
+
+
+with zipfile.ZipFile("data.zip", "r") as z_fp:
+    z_fp.extractall("./")
 
 #  set `data_dir=None` to load the complete dataset
 
 (train_dataset, val_dataset), dataset_info = tfds.load(
     "coco/2017", split=["train", "validation"], with_info=True, data_dir="data"
 )
-batch_size = 2
+batch_size = 4
 label_encoder = LabelEncoder()
 autotune = tf.data.AUTOTUNE
 train_dataset = train_dataset.map(preprocess_data, num_parallel_calls=autotune)
@@ -48,7 +45,7 @@ val_dataset = val_dataset.map(label_encoder.encode_batch, num_parallel_calls=aut
 val_dataset = val_dataset.apply(tf.data.experimental.ignore_errors())
 val_dataset = val_dataset.prefetch(autotune)
 
-epochs = 1
+epochs = 10
 
 # Running 100 training and 50 validation steps,
 # remove `.take` when training on the full dataset
@@ -76,5 +73,7 @@ tuner = RandomSearch(
     project_name="helloworld",
 )
 tuner.search_space_summary()
-tuner.search(x=train_dataset.take(20), epochs=2, validation_data=val_dataset.take(10))
+tuner.search(x=train_dataset, epochs=2, validation_data=val_dataset)
+best_model = tuner.get_best_models()[0]
+best_model.save("best_model")
 
